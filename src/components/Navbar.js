33306fe9b639c51f2599fb1a5f560d8a9279a5a1
dev_nayan <md.nayan8494@gmail.com>
2022-10-02 01:25:59 +0600

    Add zoom effect on skill and fix menu sliding

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
                     <div className="logo">
                         <a href='https://nayan-portfolio.web.app/' className='font-bold text-4xl font-signature text-white hover:text-blue-600 duration-300'>Nayan</a>
                     </div>
-                    <ul className={`menu text-white bg-gradient-to-b from-black to-gray-800 top-0 ${isOpen ? 'left-0 lg:left-2/3 lg:w-1/3' : 'left-[110%]'} absolute h-screen w-screen flex flex-col justify-center -z-10 duration-700 drop-shadow-xl lg:drop-shadow-2xl`}>
+                    <ul className={`menu text-white bg-gradient-to-b from-black to-gray-800 top-0 ${isOpen ? 'left-0 lg:left-2/3 lg:w-1/3' : 'left-[110%]'} absolute h-screen w-screen flex flex-col justify-center -z-10 duration-500 ease-in-out drop-shadow-xl lg:drop-shadow-2xl`}>
                         {
                             navLink.map(({id, link}) => (
                                 <li key={id} onClick={hamburger} className='text-2xl text-center my-3 cursor-pointer hover:text-blue-600 font-medium duration-300 xl:my-4 2xl:text-4xl'><Link to={link} smooth duration={700} offset={-15} onClick={hamburger} className="block">{link}</Link></li>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,18 +11,18 @@ const Skills = () => {
                 
                 {/* Skills with Icons */}
                 <div className='text-white grid grid-cols-3 md:grid-cols-4 md:gap-x-3 lg:grid-cols-6 lg:gap-x-5'>
-                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6'><FaHtml5 className='text-5xl mx-auto mb-2 text-orange-600 duration-300 2xl:text-7xl 2xl:mb-5'/> HTML 5</div>
-                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6'><FaCss3Alt className='text-5xl mx-auto mb-2 text-blue-500 duration-300 2xl:text-7xl 2xl:mb-5'/>CSS 3</div>
-                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6'><SiJavascript className='text-5xl mx-auto mb-2 text-yellow-400 duration-300 2xl:text-7xl 2xl:mb-5'/>JavaScript</div>
-                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6'><FaSass className='text-5xl mx-auto mb-2 text-pink-500 duration-300 2xl:text-7xl 2xl:mb-5'/>SASS</div>
-                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6'><FaBootstrap className='text-5xl mx-auto mb-2 text-violet-600 duration-300 2xl:text-7xl 2xl:mb-5'/>Bootstrap</div>
-                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6'><SiTailwindcss className='text-5xl mx-auto mb-2 text-blue-500 duration-300 2xl:text-7xl 2xl:mb-5'/>Tailwind</div>
-                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6'><FaReact className='text-5xl mx-auto mb-2 text-blue-400 duration-300 2xl:text-7xl 2xl:mb-5'/>React JS</div>
-                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6'><FaNodeJs className='text-5xl mx-auto mb-2 text-green-600 duration-300 2xl:text-7xl 2xl:mb-5'/>Node JS</div>
-                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6'><SiExpress className='text-5xl mx-auto mb-2 text-white duration-300 2xl:text-7xl 2xl:mb-5'/>Express JS</div>
-                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6'><SiMongodb className='text-5xl mx-auto mb-2 text-green-400 duration-300 2xl:text-7xl 2xl:mb-5'/>MongoDB</div>
-                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6'><FaGithub className='text-5xl mx-auto mb-2 text-white duration-300 2xl:text-7xl 2xl:mb-5'/>GitHub</div>
-                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6'><SiFirebase className='text-5xl mx-auto mb-2 text-yellow-500 duration-300 2xl:text-7xl 2xl:mb-5'/>Firebase</div>
+                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6 hover:scale-110 duration-300 cursor-pointer'><FaHtml5 className='text-5xl mx-auto mb-2 text-orange-600 duration-300 2xl:text-7xl 2xl:mb-5'/> HTML 5</div>
+                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6 hover:scale-110 duration-300 cursor-pointer'><FaCss3Alt className='text-5xl mx-auto mb-2 text-blue-500 duration-300 2xl:text-7xl 2xl:mb-5'/>CSS 3</div>
+                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6 hover:scale-110 duration-300 cursor-pointer'><SiJavascript className='text-5xl mx-auto mb-2 text-yellow-400 duration-300 2xl:text-7xl 2xl:mb-5'/>JavaScript</div>
+                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6 hover:scale-110 duration-300 cursor-pointer'><FaSass className='text-5xl mx-auto mb-2 text-pink-500 duration-300 2xl:text-7xl 2xl:mb-5'/>SASS</div>
+                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6 hover:scale-110 duration-300 cursor-pointer'><FaBootstrap className='text-5xl mx-auto mb-2 text-violet-600 duration-300 2xl:text-7xl 2xl:mb-5'/>Bootstrap</div>
+                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6 hover:scale-110 duration-300 cursor-pointer'><SiTailwindcss className='text-5xl mx-auto mb-2 text-blue-500 duration-300 2xl:text-7xl 2xl:mb-5'/>Tailwind</div>
+                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6 hover:scale-110 duration-300 cursor-pointer'><FaReact className='text-5xl mx-auto mb-2 text-blue-400 duration-300 2xl:text-7xl 2xl:mb-5'/>React JS</div>
+                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6 hover:scale-110 duration-300 cursor-pointer'><FaNodeJs className='text-5xl mx-auto mb-2 text-green-600 duration-300 2xl:text-7xl 2xl:mb-5'/>Node JS</div>
+                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6 hover:scale-110 duration-300 cursor-pointer'><SiExpress className='text-5xl mx-auto mb-2 text-white duration-300 2xl:text-7xl 2xl:mb-5'/>Express JS</div>
+                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6 hover:scale-110 duration-300 cursor-pointer'><SiMongodb className='text-5xl mx-auto mb-2 text-green-400 duration-300 2xl:text-7xl 2xl:mb-5'/>MongoDB</div>
+                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6 hover:scale-110 duration-300 cursor-pointer'><FaGithub className='text-5xl mx-auto mb-2 text-white duration-300 2xl:text-7xl 2xl:mb-5'/>GitHub</div>
+                    <div className='my-4 inline-block text-center py-2 shadow-lg 2xl:text-2xl 2xl:my-6 hover:scale-110 duration-300 cursor-pointer'><SiFirebase className='text-5xl mx-auto mb-2 text-yellow-500 duration-300 2xl:text-7xl 2xl:mb-5'/>Firebase</div>
                 </div>
             </div>
         </section>
